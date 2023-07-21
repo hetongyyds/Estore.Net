df091b12733be7bb76ff277ed7dbb5970620d643
hetongyyds <hwan223@aucklanduni.ac.nz>
2023-07-22 03:08:20 +1000

    end of section 1

---
+++ API/Entity/Product.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entity
+{
+    public class Product
+    {
+        // primary key of the entity product
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public long Price { get; set; }
+
+        public string PictureUrl { get; set; }
+
+        public string Type { get; set; }
+
+        public string Brand { get; set; }
+
+        public int QuantityInStock { get; set; }
+        
+    }
+}
